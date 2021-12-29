473266b9e1df3245df9bce40e16888c1077571d0
milekey <hata@mihr.net>
2021-12-29 17:12:23 +0900

    update icon

--- app/src/main/java/com/scaredeer/opengl/Renderer.kt
+++ app/src/main/java/com/scaredeer/opengl/Renderer.kt
@@ -9,6 +9,15 @@ import android.opengl.Matrix
 import android.util.Log
 import javax.microedition.khronos.egl.EGLConfig
 
+/**
+ * ゲームのメインループに相当するクラス
+ * （もちろん、画面の更新を中心としたもので、ゲームモデルの論理的なループとは必ずしも同じではないが、
+ * 実用上はこのクラスを中心に構成していいと思う）
+ *
+ * MainActivity で implement して記述を統合することも全然可能だが、
+ * MainActivity はその他の UI のコードなども盛り込まれることになるので、コードの見通しが悪くなり、
+ * あまり実用的ではないので、素直に分離している。
+ */
 class Renderer(context: Context?) : GLSurfaceView.Renderer {
 
     companion object {
